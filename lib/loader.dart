cee5b204030a6e627d65133bd90878cec3ab87a3
develiny <develiny9@gmail.com>
2021-12-23 17:06:52 +0900

    c21

--- lib/loader.dart
+++ lib/loader.dart
@@ -11,48 +11,30 @@ class Loader {
     }
   }
 
-  // static void showLoadingDialog(BuildContext context) async {
-  //   _context = context;
-  //   _isLoading = true;
-  //   await showDialog(
-  //       context: _context,
-  //       barrierDismissible: false,
-  //       builder: (BuildContext context) {
-  //         return SimpleDialog(
-  //           elevation: 0.0,
-  //           backgroundColor: Colors.transparent,
-  //           children: <Widget>[
-  //             Center(
-  //               child: CircularProgressIndicator(
-  //                 valueColor: AlwaysStoppedAnimation<Color>(Colors.greenAccent),
-  //               ),
-  //             )
-  //           ],
-  //         );
-  //       });
-  // }
-
   static void showLoadingDialog(BuildContext context) async {
     _context = context;
-    _isLoading = true;
-    await showDialog(
-        context: _context,
-        barrierDismissible: false,
-        builder: (BuildContext context) {
-          return WillPopScope(
-            onWillPop: () async => false,
-            child: SimpleDialog(
-              elevation: 0.0,
-              backgroundColor: Colors.transparent,
-              children: <Widget>[
-                Center(
-                  child: CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation<Color>(Colors.greenAccent),
-                  ),
-                )
-              ],
-            ),
-          );
-        });
+
+    if (!_isLoading) {
+      _isLoading = true;
+      await showDialog(
+          context: _context,
+          barrierDismissible: false,
+          builder: (BuildContext context) {
+            return WillPopScope(
+              onWillPop: () async => false,
+              child: SimpleDialog(
+                elevation: 0.0,
+                backgroundColor: Colors.transparent,
+                children: <Widget>[
+                  Center(
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.greenAccent),
+                    ),
+                  )
+                ],
+              ),
+            );
+          });
+    }
   }
 }
--- lib/login/email_login.dart
+++ lib/login/email_login.dart
@@ -16,15 +16,22 @@ class EmailLogin extends StatelessWidget {
     return SafeArea(
       child: WillPopScope(
         child: Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                CustomAppbar(),
-                EditEmailLogin(),
-                KeyboardVisibilityProvider(child: BottomNav())
-              ],
-            ),
+          body: Stack(
+            children: [
+              Positioned(
+                child: CustomAppbar(),
+                top: 0,
+                right: 0,
+                left: 0,
+              ),
+              Center(child: KeyboardVisibilityProvider(child: EditEmailLogin())),
+              Positioned(
+                child: KeyboardVisibilityProvider(child: BottomNav()),
+                bottom: 0,
+                right: 0,
+                left: 0,
+              ),
+            ],
           ),
         ),
         onWillPop: () {
@@ -54,151 +61,155 @@ class _EditEmailLoginState extends State<EditEmailLogin> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 20.0),
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                  spreadRadius: 1,
-                  blurRadius: 1,
-                  offset: Offset(1, 1),
-                  color: Colors.grey)
-            ]),
-        child: Column(
-          children: [
-            Column(
-              children: [
-                Text("Email 로그인",
-                    style:
-                        TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold)),
-                SizedBox(height: 3.0),
-                Container(
-                  height: 3,
-                  width: 90,
-                  color: Colors.orangeAccent,
-                )
-              ],
-            ),
-            SizedBox(height: 20.0),
-            TextField(
-              onChanged: (text) {
-                if (text.length != 0) {
-                  setState(() {
-                    emailtext = "";
-                  });
-                }
-              },
-              decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.email_rounded,
-                    color: Colors.grey,
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey),
-                      borderRadius: BorderRadius.all(Radius.circular(30.0))),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.indigoAccent),
-                      borderRadius: BorderRadius.all(Radius.circular(30.0))),
-                  hintText: "이메일",
-                  hintStyle: TextStyle(color: Colors.grey),
-                  contentPadding: EdgeInsets.all(10.0)),
-              controller: email,
-              autofocus: false,
-            ),
-            Row(
-              children: [
-                Padding(
-                    padding: EdgeInsets.only(left: 20.0, top: 5.0),
-                    child: Text(emailtext, style: TextStyle(color: Colors.red)))
-              ],
-            ),
-            SizedBox(height: 15.0),
-            TextField(
-              onChanged: (text) {
-                if (text.length != 0) {
-                  setState(() {
-                    passwordtext = "";
-                  });
-                }
-              },
-              obscureText: true,
-              decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.password,
-                    color: Colors.grey,
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.grey),
-                      borderRadius: BorderRadius.all(Radius.circular(30.0))),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.indigoAccent),
-                      borderRadius: BorderRadius.all(Radius.circular(30.0))),
-                  hintText: "비밀번호",
-                  hintStyle: TextStyle(color: Colors.grey),
-                  contentPadding: EdgeInsets.all(10.0)),
-              controller: email,
-              autofocus: false,
-            ),
-            Row(
-              children: [
-                Padding(
-                    padding: EdgeInsets.only(left: 20.0, top: 5.0),
-                    child:
-                        Text(passwordtext, style: TextStyle(color: Colors.red)))
-              ],
-            ),
-            SizedBox(height: 15.0),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.indigo,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0))),
-              child: Text("로그인"),
-              onPressed: () async {
-                Loader.showLoadingDialog(context);
-                if (email.text.length == 0) {
-                  setState(() {
-                    emailtext = "이메일을 입력해주세요";
-                  });
-                } else if (!email.text.contains(new RegExp(
-                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'))) {
-                  setState(() {
-                    emailtext = "올바른 이메일을 입력해주세요.";
-                  });
-                } else {
-                  try {
-                    await FirebaseAuth.instance.signInWithEmailAndPassword(
-                        email: email.text, password: password.text);
-                  } on FirebaseAuthException catch (e) {
-                    if (e.code == 'user-not-found') {
-                      //회원없음
-                      print('No user found for that email.');
-                    } else if (e.code == 'wrong-password') {
-                      //비번틀림
-                      print('Wrong password provided for that user.');
-                      setState(() {
-                        passwordtext = "비밀번호가 틀립니다.";
-                      });
+    final bool isKeyboardVisible = KeyboardVisibilityProvider.isKeyboardVisible(context);
+    return Padding(
+      padding: isKeyboardVisible ? EdgeInsets.only(top: 70.0) : EdgeInsets.symmetric(vertical: 70.0),
+      child: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20.0),
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 30.0),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                    spreadRadius: 1,
+                    blurRadius: 1,
+                    offset: Offset(1, 1),
+                    color: Colors.grey)
+              ]),
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  Text("Email 로그인",
+                      style:
+                          TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold)),
+                  SizedBox(height: 3.0),
+                  Container(
+                    height: 3,
+                    width: 90,
+                    color: Colors.orangeAccent,
+                  )
+                ],
+              ),
+              SizedBox(height: 20.0),
+              TextField(
+                onChanged: (text) {
+                  if (text.length != 0) {
+                    setState(() {
+                      emailtext = "";
+                    });
+                  }
+                },
+                decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.email_rounded,
+                      color: Colors.grey,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.indigoAccent),
+                        borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                    hintText: "이메일",
+                    hintStyle: TextStyle(color: Colors.grey),
+                    contentPadding: EdgeInsets.all(10.0)),
+                controller: email,
+                autofocus: false,
+              ),
+              Row(
+                children: [
+                  Padding(
+                      padding: EdgeInsets.only(left: 20.0, top: 5.0),
+                      child: Text(emailtext, style: TextStyle(color: Colors.red)))
+                ],
+              ),
+              SizedBox(height: 15.0),
+              TextField(
+                onChanged: (text) {
+                  if (text.length != 0) {
+                    setState(() {
+                      passwordtext = "";
+                    });
+                  }
+                },
+                obscureText: true,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.password,
+                      color: Colors.grey,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.indigoAccent),
+                        borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                    hintText: "비밀번호",
+                    hintStyle: TextStyle(color: Colors.grey),
+                    contentPadding: EdgeInsets.all(10.0)),
+                controller: email,
+                autofocus: false,
+              ),
+              Row(
+                children: [
+                  Padding(
+                      padding: EdgeInsets.only(left: 20.0, top: 5.0),
+                      child:
+                          Text(passwordtext, style: TextStyle(color: Colors.red)))
+                ],
+              ),
+              SizedBox(height: 15.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.indigo,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0))),
+                child: Text("로그인"),
+                onPressed: () async {
+                  Loader.showLoadingDialog(context);
+                  if (email.text.length == 0) {
+                    setState(() {
+                      emailtext = "이메일을 입력해주세요";
+                    });
+                  } else if (!email.text.contains(new RegExp(
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'))) {
+                    setState(() {
+                      emailtext = "올바른 이메일을 입력해주세요.";
+                    });
+                  } else {
+                    try {
+                      await FirebaseAuth.instance.signInWithEmailAndPassword(
+                          email: email.text, password: password.text);
+                    } on FirebaseAuthException catch (e) {
+                      if (e.code == 'user-not-found') {
+                        //회원없음
+                        print('No user found for that email.');
+                      } else if (e.code == 'wrong-password') {
+                        //비번틀림
+                        print('Wrong password provided for that user.');
+                        setState(() {
+                          passwordtext = "비밀번호가 틀립니다.";
+                        });
+                      }
                     }
                   }
-                }
-                Loader.closeLoadingDialog();
-              },
-            ),
-            SizedBox(height: 15.0),
-            TextButton(onPressed: () {}, child: Text("비밀번호를 잊으셨나요?")),
-            TextButton(
-                onPressed: () {
-                  setState(() {
-                    Loader.showLoadingDialog(context);
-                  });
+                  Loader.closeLoadingDialog();
                 },
-                child: Text("test")),
-          ],
+              ),
+              SizedBox(height: 15.0),
+              TextButton(onPressed: () {}, child: Text("비밀번호를 잊으셨나요?")),
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      Loader.showLoadingDialog(context);
+                    });
+                  },
+                  child: Text("test")),
+            ],
+          ),
         ),
       ),
     );
