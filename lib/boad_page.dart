bfc62424051749bee0e8b4822a5ba843f94f3976
develiny <develiny9@gmail.com>
2021-12-30 23:52:10 +0900

    c30

--- lib/boad_page.dart
+++ lib/boad_page.dart
@@ -1,5 +1,7 @@
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:html_editor_enhanced/html_editor.dart';
 import 'package:metagamer/current_route.dart';
@@ -47,7 +49,7 @@ class _BoadPageState extends State<BoadPage> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 CustomAppbar(),
-                Boad(),
+                BoadTest(),
                 KeyboardVisibilityProvider(child: BottomNav())
               ],
             ),
@@ -68,98 +70,146 @@ class Boad extends StatefulWidget {
 class _BoadState extends State<Boad> {
   HtmlEditorController controller = HtmlEditorController();
 
+  String pp = "";
+
   @override
   Widget build(BuildContext context) {
-    return HtmlEditor(
-      controller: controller,
-      htmlEditorOptions: HtmlEditorOptions(
-        hint: "Your text here...",
-      ),
-      htmlToolbarOptions: HtmlToolbarOptions(
-        defaultToolbarButtons: [
-          FontButtons(),
-          ColorButtons(),
-          InsertButtons(
-              video: false,
-              audio: false,
-              table: false,
-              hr: false,
-              otherFile: false)
-        ],
-        mediaUploadInterceptor: (PlatformFile file, InsertFileType type) async {
-          print(file.name); //filename
-          print(file.size); //size in bytes
-          print(file.extension); //file extension (eg jpeg or mp4)
-          return true;
-        },
-      ),
+    return Column(
+      children: [
+        HtmlEditor(
+          controller: controller,
+          htmlEditorOptions: HtmlEditorOptions(
+            hint: "Your text here...",
+          ),
+          htmlToolbarOptions: HtmlToolbarOptions(
+            defaultToolbarButtons: [
+              FontButtons(),
+              ColorButtons(),
+              InsertButtons(
+                  video: false,
+                  audio: false,
+                  table: false,
+                  hr: false,
+                  otherFile: false)
+            ],
+            mediaUploadInterceptor:
+                (PlatformFile file, InsertFileType type) async {
+              print(file.name); //filename
+              print(file.size); //size in bytes
+              print(file.extension); //file extension (eg jpeg or mp4)
+              return true;
+            },
+          ),
 
-
-      // callbacks: Callbacks(onBeforeCommand: (String? currentHtml) {
-      //   print('html before change is $currentHtml');
-      // }, onChangeContent: (String? changed) {
-      //   print('content changed to $changed');
-      // }, onChangeCodeview: (String? changed) {
-      //   print('code changed to $changed');
-      // }, onChangeSelection: (EditorSettings settings) {
-      //   print('parent element is ${settings.parentElement}');
-      //   print('font name is ${settings.fontName}');
-      // }, onDialogShown: () {
-      //   print('dialog shown');
-      // }, onEnter: () {
-      //   print('enter/return pressed');
-      // }, onFocus: () {
-      //   print('editor focused');
-      // }, onBlur: () {
-      //   print('editor unfocused');
-      // }, onBlurCodeview: () {
-      //   print('codeview either focused or unfocused');
-      // }, onInit: () {
-      //   print('init');
-      // },
-      //     //this is commented because it overrides the default Summernote handlers
-      //     /*onImageLinkInsert: (String? url) {
-      //               print(url ?? "unknown url");
-      //             },
-      //             onImageUpload: (FileUpload file) async {
-      //               print(file.name);
-      //               print(file.size);
-      //               print(file.type);
-      //               print(file.base64);
-      //             },*/
-      //     onKeyDown: (int? keyCode) {
-      //   print('$keyCode key downed');
-      //   print('current character count: ${controller.characterCount}');
-      // }, onKeyUp: (int? keyCode) {
-      //   print('$keyCode key released');
-      // }, onMouseDown: () {
-      //   print('mouse downed');
-      // }, onMouseUp: () {
-      //   print('mouse released');
-      // }, onNavigationRequestMobile: (String url) {
-      //   print(url);
-      //   return NavigationActionPolicy.ALLOW;
-      // }, onPaste: () {
-      //   print('pasted into editor');
-      // }, onScroll: () {
-      //   print('editor scrolled');
-      // }),
-      // plugins: [
-      //   SummernoteAtMention(
-      //       getSuggestionsMobile: (String value) {
-      //         var mentions = <String>['test1', 'test2', 'test3'];
-      //         return mentions
-      //             .where((element) => element.contains(value))
-      //             .toList();
-      //       },
-      //       mentionsWeb: ['test1', 'test2', 'test3'],
-      //       onSelect: (String value) {
-      //         print(value);
-      //       }),
-      // ],
-
-
-
+          // callbacks: Callbacks(onBeforeCommand: (String? currentHtml) {
+          //   print('html before change is $currentHtml');
+          // }, onChangeContent: (String? changed) {
+          //   print('content changed to $changed');
+          // }, onChangeCodeview: (String? changed) {
+          //   print('code changed to $changed');
+          // }, onChangeSelection: (EditorSettings settings) {
+          //   print('parent element is ${settings.parentElement}');
+          //   print('font name is ${settings.fontName}');
+          // }, onDialogShown: () {
+          //   print('dialog shown');
+          // }, onEnter: () {
+          //   print('enter/return pressed');
+          // }, onFocus: () {
+          //   print('editor focused');
+          // }, onBlur: () {
+          //   print('editor unfocused');
+          // }, onBlurCodeview: () {
+          //   print('codeview either focused or unfocused');
+          // }, onInit: () {
+          //   print('init');
+          // },
+          //     //this is commented because it overrides the default Summernote handlers
+          //     /*onImageLinkInsert: (String? url) {
+          //               print(url ?? "unknown url");
+          //             },
+          //             onImageUpload: (FileUpload file) async {
+          //               print(file.name);
+          //               print(file.size);
+          //               print(file.type);
+          //               print(file.base64);
+          //             },*/
+          //     onKeyDown: (int? keyCode) {
+          //   print('$keyCode key downed');
+          //   print('current character count: ${controller.characterCount}');
+          // }, onKeyUp: (int? keyCode) {
+          //   print('$keyCode key released');
+          // }, onMouseDown: () {
+          //   print('mouse downed');
+          // }, onMouseUp: () {
+          //   print('mouse released');
+          // }, onNavigationRequestMobile: (String url) {
+          //   print(url);
+          //   return NavigationActionPolicy.ALLOW;
+          // }, onPaste: () {
+          //   print('pasted into editor');
+          // }, onScroll: () {
+          //   print('editor scrolled');
+          // }),
+          // plugins: [
+          //   SummernoteAtMention(
+          //       getSuggestionsMobile: (String value) {
+          //         var mentions = <String>['test1', 'test2', 'test3'];
+          //         return mentions
+          //             .where((element) => element.contains(value))
+          //             .toList();
+          //       },
+          //       mentionsWeb: ['test1', 'test2', 'test3'],
+          //       onSelect: (String value) {
+          //         print(value);
+          //       }),
+          // ],
+        ),
+        TextButton(
+            onPressed: () async {
+              print(await controller.getText());
+            },
+            child: Text("test"))
+      ],
     );
   }
+}
+
+class BoadTest extends StatefulWidget {
+  const BoadTest({Key? key}) : super(key: key);
+
+  @override
+  _BoadTestState createState() => _BoadTestState();
+}
+
+class _BoadTestState extends State<BoadTest> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        height: 500,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 200,
+              child: InAppWebView(
+                initialUrlRequest: URLRequest(
+                    url: Uri.parse(
+                        "https://firebasestorage.googleapis.com/v0/b/metagamer-8d6a1.appspot.com/o/testfolder%2Ftesthtml.html?alt=media&token=ac95f610-6e29-4918-bd2f-ea5ff52849b5")),
+              ),
+            ),
+            SizedBox(
+              height: 200,
+              child: Html(
+                data: testString + testString2,
+              ),
+            )
+          ],
+        ));
+  }
+
+  String testString = "<p>11문단입니다</p><p>22문단입니다</p>";
+  String testString2 = '<img src = "https://firebasestorage.googleapis.com/v0/b/metagamer-8d6a1.appspot.com/o/dev%2Fdefaultprofileicon.png?alt=media&token=cee38d5d-48b1-4e85-bbe0-d3114c07959a"/>';
+  var htmldata = """
+    <p>1문단입니다</p>
+    <p>2문단입니다</p>
+  """;
 }
