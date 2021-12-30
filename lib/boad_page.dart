814d6642e469fc9a239660a4856a4801b801d04b
develiny <develiny9@gmail.com>
2021-12-30 17:39:35 +0900

    c29

--- lib/boad_page.dart
+++ lib/boad_page.dart
@@ -1,5 +1,7 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:html_editor_enhanced/html_editor.dart';
 import 'package:metagamer/current_route.dart';
 
 import 'appbar.dart';
@@ -31,7 +33,8 @@ class _BoadPageState extends State<BoadPage> {
           final cantExit = timegap >= Duration(seconds: 2);
           pre_backpress = DateTime.now();
           if (cantExit) {
-            final snack = SnackBar(content: Text("한번더눌러서종료"), duration: Duration(seconds: 2));
+            final snack = SnackBar(
+                content: Text("한번더눌러서종료"), duration: Duration(seconds: 2));
             ScaffoldMessenger.of(context).showSnackBar(snack);
             return false;
           } else {
@@ -63,9 +66,100 @@ class Boad extends StatefulWidget {
 }
 
 class _BoadState extends State<Boad> {
+  HtmlEditorController controller = HtmlEditorController();
+
   @override
   Widget build(BuildContext context) {
-    return Expanded(child: SingleChildScrollView(child: Center(child: Text("boad"),),));
+    return HtmlEditor(
+      controller: controller,
+      htmlEditorOptions: HtmlEditorOptions(
+        hint: "Your text here...",
+      ),
+      htmlToolbarOptions: HtmlToolbarOptions(
+        defaultToolbarButtons: [
+          FontButtons(),
+          ColorButtons(),
+          InsertButtons(
+              video: false,
+              audio: false,
+              table: false,
+              hr: false,
+              otherFile: false)
+        ],
+        mediaUploadInterceptor: (PlatformFile file, InsertFileType type) async {
+          print(file.name); //filename
+          print(file.size); //size in bytes
+          print(file.extension); //file extension (eg jpeg or mp4)
+          return true;
+        },
+      ),
+
+
+      // callbacks: Callbacks(onBeforeCommand: (String? currentHtml) {
+      //   print('html before change is $currentHtml');
+      // }, onChangeContent: (String? changed) {
+      //   print('content changed to $changed');
+      // }, onChangeCodeview: (String? changed) {
+      //   print('code changed to $changed');
+      // }, onChangeSelection: (EditorSettings settings) {
+      //   print('parent element is ${settings.parentElement}');
+      //   print('font name is ${settings.fontName}');
+      // }, onDialogShown: () {
+      //   print('dialog shown');
+      // }, onEnter: () {
+      //   print('enter/return pressed');
+      // }, onFocus: () {
+      //   print('editor focused');
+      // }, onBlur: () {
+      //   print('editor unfocused');
+      // }, onBlurCodeview: () {
+      //   print('codeview either focused or unfocused');
+      // }, onInit: () {
+      //   print('init');
+      // },
+      //     //this is commented because it overrides the default Summernote handlers
+      //     /*onImageLinkInsert: (String? url) {
+      //               print(url ?? "unknown url");
+      //             },
+      //             onImageUpload: (FileUpload file) async {
+      //               print(file.name);
+      //               print(file.size);
+      //               print(file.type);
+      //               print(file.base64);
+      //             },*/
+      //     onKeyDown: (int? keyCode) {
+      //   print('$keyCode key downed');
+      //   print('current character count: ${controller.characterCount}');
+      // }, onKeyUp: (int? keyCode) {
+      //   print('$keyCode key released');
+      // }, onMouseDown: () {
+      //   print('mouse downed');
+      // }, onMouseUp: () {
+      //   print('mouse released');
+      // }, onNavigationRequestMobile: (String url) {
+      //   print(url);
+      //   return NavigationActionPolicy.ALLOW;
+      // }, onPaste: () {
+      //   print('pasted into editor');
+      // }, onScroll: () {
+      //   print('editor scrolled');
+      // }),
+      // plugins: [
+      //   SummernoteAtMention(
+      //       getSuggestionsMobile: (String value) {
+      //         var mentions = <String>['test1', 'test2', 'test3'];
+      //         return mentions
+      //             .where((element) => element.contains(value))
+      //             .toList();
+      //       },
+      //       mentionsWeb: ['test1', 'test2', 'test3'],
+      //       onSelect: (String value) {
+      //         print(value);
+      //       }),
+      // ],
+
+
+
+    );
   }
 }
-
